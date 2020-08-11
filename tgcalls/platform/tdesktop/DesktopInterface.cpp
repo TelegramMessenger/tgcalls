82172d1b7bef85c4dba984dc0d9e681b23dbc1a7
John Preston <johnprestonmail@gmail.com>
2020-08-11 13:41:15 +0400

    Use the same PlatformInterface on Windows and Linux.

--- tgcalls/platform/tdesktop/DesktopInterface.cpp
+++ tgcalls/platform/tdesktop/DesktopInterface.cpp
@@ -1,4 +1,4 @@
-#include "WindowsInterface.h"
+#include "DesktopInterface.h"
 
 #include "platform/tdesktop/VideoCapturerInterfaceImpl.h"
 #include "platform/tdesktop/VideoCapturerTrackSource.h"
@@ -9,32 +9,32 @@
 
 namespace tgcalls {
 
-std::unique_ptr<webrtc::VideoEncoderFactory> WindowsInterface::makeVideoEncoderFactory() {
+std::unique_ptr<webrtc::VideoEncoderFactory> DesktopInterface::makeVideoEncoderFactory() {
 	return webrtc::CreateBuiltinVideoEncoderFactory();
 }
 
-std::unique_ptr<webrtc::VideoDecoderFactory> WindowsInterface::makeVideoDecoderFactory() {
+std::unique_ptr<webrtc::VideoDecoderFactory> DesktopInterface::makeVideoDecoderFactory() {
 	return webrtc::CreateBuiltinVideoDecoderFactory();
 }
 
-rtc::scoped_refptr<webrtc::VideoTrackSourceInterface> WindowsInterface::makeVideoSource(rtc::Thread *signalingThread, rtc::Thread *workerThread) {
+rtc::scoped_refptr<webrtc::VideoTrackSourceInterface> DesktopInterface::makeVideoSource(rtc::Thread *signalingThread, rtc::Thread *workerThread) {
 	const auto videoTrackSource = VideoCapturerTrackSource::Create();
 	return videoTrackSource
 		? webrtc::VideoTrackSourceProxy::Create(signalingThread, workerThread, videoTrackSource)
 		: nullptr;
 }
 
-bool WindowsInterface::supportsEncoding(const std::string &codecName) {
+bool DesktopInterface::supportsEncoding(const std::string &codecName) {
 	return (codecName == cricket::kH264CodecName)
 		|| (codecName == cricket::kVp8CodecName);
 }
 
-std::unique_ptr<VideoCapturerInterface> WindowsInterface::makeVideoCapturer(rtc::scoped_refptr<webrtc::VideoTrackSourceInterface> source, bool useFrontCamera, std::function<void(VideoState)> stateUpdated, std::shared_ptr<PlatformContext> platformContext) {
+std::unique_ptr<VideoCapturerInterface> DesktopInterface::makeVideoCapturer(rtc::scoped_refptr<webrtc::VideoTrackSourceInterface> source, bool useFrontCamera, std::function<void(VideoState)> stateUpdated, std::shared_ptr<PlatformContext> platformContext) {
 	return std::make_unique<VideoCapturerInterfaceImpl>(source, useFrontCamera, stateUpdated);
 }
 
 std::unique_ptr<PlatformInterface> CreatePlatformInterface() {
-	return std::make_unique<WindowsInterface>();
+	return std::make_unique<DesktopInterface>();
 }
 
 } // namespace tgcalls
