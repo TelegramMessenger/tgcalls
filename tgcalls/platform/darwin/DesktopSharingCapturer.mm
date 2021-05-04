ae40919a046c6ad03b128c795207d27b10f437f0
overtake <overtakeful@gmail.com>
2021-05-04 10:49:20 +0300

    update fps

--- tgcalls/platform/darwin/DesktopSharingCapturer.mm
+++ tgcalls/platform/darwin/DesktopSharingCapturer.mm
@@ -70,7 +70,7 @@ private:
 
         tgcalls::DesktopCaptureSourceData data{
 	        /*.aspectSize = */{ 1920, 1080 },
-	        /*.fps = */18,
+	        /*.fps = */24,
 	        /*.captureMouse = */true,
         };
         renderer.emplace(captureSource, data);
