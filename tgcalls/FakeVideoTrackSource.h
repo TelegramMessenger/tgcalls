9da26b11bcdbbdb08b981a402417929c4a2118e6
Arseny Smirnov <arseny30@gmail.com>
2021-05-19 16:45:39 +0400

    refactor FakeVideoTrackSource

--- tgcalls/FakeVideoTrackSource.h
+++ tgcalls/FakeVideoTrackSource.h
@@ -4,11 +4,29 @@
 
 namespace webrtc {
 class VideoTrackSourceInterface;
+class VideoFrame;
 }
 
 namespace tgcalls {
+class FrameSource {
+public:
+  struct Info {
+    int32_t width;
+    int32_t height;
+  };
+
+  virtual ~FrameSource() = default;
+
+  virtual Info info() const = 0;
+  virtual webrtc::VideoFrame next_frame();
+  virtual void next_frame_rgb0(char *buf, double *pt_in_seconds) = 0;
+
+  static std::unique_ptr<FrameSource> chess();
+  static std::unique_ptr<FrameSource> from_file(std::string path);
+};
+
 class FakeVideoTrackSource {
  public:
-  static std::function<webrtc::VideoTrackSourceInterface*()> create();
+  static std::function<webrtc::VideoTrackSourceInterface*()> create(std::unique_ptr<FrameSource> source);
 };
 }
