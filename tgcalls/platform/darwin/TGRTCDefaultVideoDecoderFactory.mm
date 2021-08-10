81e97fa52e25b7775b62ce07bb653533d81e91b3
John Preston <johnprestonmail@gmail.com>
2021-08-10 19:13:29 +0300

    Fix build for TDesktop on macOS.

--- tgcalls/platform/darwin/TGRTCDefaultVideoDecoderFactory.mm
+++ tgcalls/platform/darwin/TGRTCDefaultVideoDecoderFactory.mm
@@ -23,7 +23,7 @@
 #import "TGRTCVideoDecoderH265.h"
 #endif
 
-#import "RTCWrappedNativeVideoDecoder.h"
+#import "sdk/objc/api/video_codec/RTCWrappedNativeVideoDecoder.h"
 #include "modules/video_coding/codecs/h264/include/h264.h"
 
 @implementation TGRTCDefaultVideoDecoderFactory
