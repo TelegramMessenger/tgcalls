af88317c7529c5b69c4d574d1293fb385abb3f02
Ali <>
2023-03-03 22:27:40 +0400

    Fix initial network state record

--- tgcalls/v2/InstanceV2Impl.cpp
+++ tgcalls/v2/InstanceV2Impl.cpp
@@ -1086,7 +1086,9 @@ public:
         beginQualityTimer(0);
         beginLogTimer(0);
 
-
+        NativeNetworkingImpl::State initialNetworkState;
+        initialNetworkState.isReadyToSendData = false;
+        onNetworkStateUpdated(initialNetworkState);
     }
 
     void beginQualityTimer(int delayMs) {
